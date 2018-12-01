0a991efb666049d52fc1044827037f5881988735
unknown <seyfettin.yumlu@vizyoneks.com.tr>
2018-12-01 16:58:02 +0300

    2. gun calismalari

---
+++ FirstNetCoreMvcProject/FirstNetCoreMvcProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstNetCoreMvcProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
